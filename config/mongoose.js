a17170521fcc82ef16698d718929d826f0f7a12f
thpss91103 <a0975142932@gmail.com>
2023-04-05 22:08:54 +0800

    feat: create all project needs files

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const { Error } = require('mongoose')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('Mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('Mongodb connected!')
+})
+
+module.exports = db
